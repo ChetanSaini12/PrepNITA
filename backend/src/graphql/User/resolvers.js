c3b12a2d06efd39e94b53899d224aebab081c4f8
Chetan Saini <chetansaini1241@gmail.com>
2024-02-11 00:34:55 +0530

    LoginUser Mutation Created

--- backend/src/graphql/User/resolvers.js
+++ backend/src/graphql/User/resolvers.js
@@ -1,6 +1,5 @@
-import { getAllUserData } from '../../controllers/allUser.controller.js'
-import { createUserInDB } from '../../controllers/createUser.controller.js'
 import {GraphQLError} from 'graphql'
+import { createUserInDB, getAllUserData, loginUser } from '../../controllers/auth.constroller.js'
 const queries = {
   getAllUser: () => getAllUserData(),
 }
@@ -9,13 +8,14 @@ const mutations = {
   createUser: (_, payload) => createUserInDB(_, payload),
   tempMut: (_, payload) => {
     console.log('PAYLOAD', payload.tempVal)
-      throw new GraphQLError('temp mut error', {
-        extensions : {
-          code : 'FORBIDDEN'
-        }
-      })
-      return payload.tempVal
-  }
+    // throw new GraphQLError('temp mut error', {
+    //   extensions : {
+    //     code : 'FORBIDDEN'
+    //   }
+    // })
+    return payload.tempVal
+  },
+  loginUser: (_, payload) => loginUser(_, payload)
 }
 
 export const resolvers = { queries, mutations }
