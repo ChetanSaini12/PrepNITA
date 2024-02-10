fe934ab7b8c8499df250faf7d025575fcc68f135
vikram110703 <sainivks4321@gmail.com>
2024-02-10 22:25:27 +0530

    error handled in backend

--- backend/src/graphql/User/resolvers.js
+++ backend/src/graphql/User/resolvers.js
@@ -1,6 +1,6 @@
 import { getAllUserData } from '../../controllers/allUser.controller.js'
 import { createUserInDB } from '../../controllers/createUser.controller.js'
-
+import {GraphQLError} from 'graphql'
 const queries = {
   getAllUser: () => getAllUserData(),
 }
@@ -9,6 +9,11 @@ const mutations = {
   createUser: (_, payload) => createUserInDB(_, payload),
   tempMut: (_, payload) => {
     console.log('PAYLOAD', payload.tempVal)
+      throw new GraphQLError('temp mut error', {
+        extensions : {
+          code : 'FORBIDDEN'
+        }
+      })
       return payload.tempVal
   }
 }
