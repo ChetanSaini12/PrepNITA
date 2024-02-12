bd2a6f98aba69a7cc7ced1ee52b413cdfdf261ce
Chetan Saini <chetansaini1241@gmail.com>
2024-02-13 00:15:10 +0530

    SignUp Page Error Fixed

--- frontend/src/Pages/AllUsers.jsx
+++ frontend/src/Pages/AllUsers.jsx
@@ -8,20 +8,7 @@ import { TEMP_MUT } from "../gqlOperatons/mutations";
 
 function AllUsers() {
   const { data, loading: queryLoading, error: queryError } = useQuery(ALL_USER);
-  const [tempData, setTempData] = useState("");
-  const [tempStr, { loading: mutationLoading, error: mutationError }] = useMutation(TEMP_MUT, {
-    variables: { tempVal: "JAI SHREE RAM!!" },
-    onCompleted: (data) => {
-      // Update state with the received string
-      setTempData(data.tempMut);
-    },
-  });
-
-  if(mutationError) 
-  {
-    return <div className='p-10'>ERROR IN MUT </div>
-  }
-
+  
   return (
     <>
       <div>AllUsers</div>
@@ -40,14 +27,6 @@ function AllUsers() {
       )}
       {queryLoading && <div>Loading...</div>}
       {queryError && <div>{queryError.message}</div>}
-      <button className="p-5"
-        onClick={tempStr}
-      >
-        CLICK
-      </button>
-      {mutationLoading && <div>Loading...</div>}
-      {mutationError && <div>{mutationError.message}</div>}
-      <div>{tempData}</div>
     </>
   );
 }
