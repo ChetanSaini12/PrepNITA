1e3ab3c1ff2510298635c53d099e0900e81db96b
Chetan Saini <chetansaini1241@gmail.com>
2024-02-13 19:32:20 +0530

    Question Mutation and Resolvers Created

--- backend/src/graphql/Questions/mutations.js
+++ backend/src/graphql/Questions/mutations.js
@@ -1,5 +1,15 @@
 export const mutations = `#graphql
     createQuestion(Question : QuestionInput) : Question 
 
+    upVoteQuestion(QuestionId : Int!, userId : Int!) : Question  
+    
+    downVoteQuestion(QuestionId : Int!, userId : Int!) : Question
+    
+    changeApproveStatusOfQue(QuestionId : Int!) : Question
+
+    deleteQuestion(QuestionId : Int!) : String
+
+    updateQuestion(QuestionId : Int!, Question : QuestionInput) : Question
+
     tempQuestion(tempVal : String) : String
 `
