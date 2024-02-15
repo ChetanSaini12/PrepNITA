7f4b4fe9033ca4d882f3ae18c1c5df6ce2b5c9a7
vikram110703 <sainivks4321@gmail.com>
2024-02-15 15:20:20 +0530

    searchBar and Question page functionality added

--- frontend/src/Components/SearchBar.jsx
+++ frontend/src/Components/SearchBar.jsx
@@ -1,48 +1,51 @@
-// import React from "react";
+import { Button } from "flowbite-react";
+import React from "react";
 
-// function SearchBar(props) {
-//   return (
-//     <>
-//       <div>SearchBarprops</div>
-//       <div>
-//         <div>
-//           <select
-//             onChange={(e) => {
-//               props.setFilter;
-//               e.target.value;
-//             }}
-//           >
-//             <option selected disabled>
-//               Select an option to search
-//             </option>
-//             {props.searchOptions.map((choice, index) => (
-//               <option
-//                 key={index}
-//                 value={index}
-//                 selected={index === props.filter ? "selected" : ""}
-//               >
-//                 {choice}
-//               </option>
-//             ))}
-//           </select>
-//           <div>
-//             <input
-//               type="text"
-//               placeholder="Enter to search"
-//               value={props.text}
-//               className=""
-//               onChange={(e) => {props.setText(e.target.value)}} 
-//             ></input>
-//           </div>
-//           <div>
-//             <button onClick={props.handleClickSearch}>
-//                 Search
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+function SearchBar(prop) {
+  const props = prop.props;
+  // console.log("SearchBar props",props);
+  return (
+    <>
+      <div>SearchBar props</div>
+      <div>
+        <div>
+          <select
+            onChange={(e) => {
+              props.setFilter(
+                e.target.value);
+            }}
+            className="text-black"
+          >
+            <option selected disabled>
+              Select an option to search
+            </option>
+            {props.search_options?.map((choice, index) => (
+              <option
+                key={index}
+                value={index}
+                selected={index === props.filter ? "selected" : ""}
+              >
+                {choice}
+              </option>
+            ))}
+          </select>
+          <div>
+            <input className='text-black'
+              type="text"
+              placeholder="Enter to search"
+              value={props.text}
+              onChange={(e) => { props.setText(e.target.value.trim()) }}
+            ></input>
+          </div>
+          <div>
+            <Button onClick={props.handleClickSearch} gradientDuoTone="purpleToPink" className='mb-2 mt-2' >
+              Search
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default SearchBar;
+export default SearchBar;
