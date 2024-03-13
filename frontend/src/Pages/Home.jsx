60057ebe958215c5e5ab6e017ebcf74a0a6589ea
chandan1971 <chandandey1357@gmail.com>
2024-03-13 19:41:34 +0530

    Added hame page bg

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,11 +2,19 @@ import React, { useEffect } from 'react';
 import { VerifyToken } from '../utils/verifyToken';
 import { useDispatch, useSelector } from 'react-redux';
 import { LogoutUser } from '../app/user/userSlice';
+import Pic from '../Assets/HomePagePic.jpg'
+import toast from 'react-hot-toast';
+
 function Home() {
   return (
-    <div className=' w-screen h-screen border-spacing-0'>
-      <div >Home</div>
-      <div>Hello World</div>
+    toast.success("Welcome to the Home Page",{duration: 2000}),
+    <div className='w-screen h-screen border-spacing-0' style={
+      {backgroundImage: `url(${Pic})`,
+    backgroundRepeat: 'no-repeat',
+  backgroundSize:'cover',alignContent:'center'}
+    }>
+
+    
     </div>
   )
 }
