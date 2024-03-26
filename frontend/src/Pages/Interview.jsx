16f2c36ac470eac9775ac35b03cb2a87901d2c33
vikram110703 <sainivks4321@gmail.com>
2024-03-26 21:12:53 +0530

    interview page updated

--- frontend/src/Pages/Interview.jsx
+++ frontend/src/Pages/Interview.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SidebarComponent from '../Components/Sidebar'
 import { Button, TextInput } from 'flowbite-react'
 import DateTimePicker from '../Components/DatePicker'
 import moment from 'moment';
-import { CREATE_INTERVIEW } from '../gqlOperatons/Interview/mutations';
+import { CREATE_INTERVIEW, GET_INTERVIEW } from '../gqlOperatons/Interview/mutations';
 import toast from 'react-hot-toast';
 import { useMutation } from '@apollo/client';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Loader } from './Loader';
+import { setLoading } from '../app/user/userSlice';
 
 function Interviews() {
+  const { loggedIn, isLoading } = useSelector((state) => state.user);
   const [dateTime, setDateTime] = useState(new Date());
   const [Duration, setDuration] = useState(30);
   const [Topics, setTopics] = useState("");
-  const [Error,setError]=useState(null);
+  const [Error, setError] = useState(null);
+  const [data, setdata] = useState(null);
+  const [interviews, setInterviews] = useState(null);
+  const [refresh, setRefresh] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+
+
+
 
   const [createInterview] = useMutation(CREATE_INTERVIEW, {
     onError: (error) => {
@@ -20,9 +35,36 @@ function Interviews() {
     },
   });
 
+  const [getInterviews] = useMutation(GET_INTERVIEW, {
+    onError: (error) => {
+      console.log("error in getting interviews ", error);
+      setError(error.message);
+    },
+  });
+
+
+
+  useEffect(() => {
+    // console.log(loggedIn);
+    getInterviews().then((data) => {
+      console.log("Interviews data ", data);
+      setInterviews(data.data.getInterview);
+    })
+      .catch((error) => {
+        console.log("Error in getting interviews ", error);
+        setError(error.message);
+
+        // setError(error.message);
+      });
+    // setdata(null);
+
+  }, [data]);
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("interview Data ", dateTime.toISOString(), " ", Duration);
+    // console.log("interview Data ", dateTime.toISOString(), " ", Duration);
+    dispatch(setLoading(true));
     createInterview({
       variables: {
         Interview: {
@@ -31,26 +73,54 @@ function Interviews() {
           topics: Topics?.split(','),
         }
       },
-    }).then((data) => {
-      console.log("Interview created ", data);
-      if (data.errors) setError(data.errors ? data.errors.message : "Error in creating interview");
-      else
-        toast.success("Interview created successfully");
+    }).then((res) => {
+      console.log("Interview created ", res);
+      if (res.errors) setError(res.errors ? res.errors.message : "Error in creating interview");
+      else {
+
+        setdata(res.data.createInterview);
+      }
+      dispatch(setLoading(false));
+
     }).catch((error) => {
       console.log("Error in creating interview ", error);
       setError(error.message);
+      dispatch(setLoading(false));
     });
 
   };
-  if(Error) toast.error(Error);
-  else toast.
+
+
+  if (isLoading) return <Loader />;
+  if (!isLoading && !loggedIn) {
+    return navigate('/register');
+  }
+  // console.log("data ", data);
+  if (Error) {
+    toast.error(Error);
+    // setTimeout(() => {
+    setError(null);
+    setdata(null);
+
+    // }, 2000);
+    // setRefresh(!refresh);
+  }
+  else if (data) {
+    toast.success("Interview created successfully");
+    // setTimeout(() => {
+    setdata(null);
+    setError(null);
+    // }, 20000);
+    // setRefresh(!refresh);
+  }
+
   return (
     <div className=' w-screen h-screen border-spacing-0'>
       {/* <SidebarComponent></SidebarComponent> */}
-      <div>
+      <div className='my-5'>
         <form onSubmit={handleSubmit}>
-          <TextInput placeholder='Duration' value={Duration}
-            required onChange={(e) => { setDuration(e.target.value) }}
+          <TextInput type='number' placeholder='Duration' value={Duration}
+            required onChange={(e) => { setDuration(parseInt(e.target.value)) }}
           ></TextInput>
           <TextInput placeholder='Topics (format :cs fundamentals, os , ...)' value={Topics}
             onChange={(e) => { setTopics(e.target.value) }}
@@ -59,7 +129,26 @@ function Interviews() {
           <Button type='submit'>Create</Button>
         </form>
       </div>
+
+      <div>
+        <h1 className='text-5xl'>Your Interviews</h1>
+        <div className='flex gap-5 flex-wrap'>
+          {interviews?.map((interview) => {
+            return (
+              <div className='border-2 border-black p-3'>
+                <h1>Interviewee : {interview.intervieweeName}</h1>
+                <h1>Interviewer : {interview.interviewerName}</h1>
+                <h1>Start Time : {moment(interview.startTime).format('MMMM Do YYYY, h:mm:ss a')}</h1>
+                <h1>Duration : {interview.duration}</h1>
+                <h1>Topics : {interview.topics?.join(', ')}</h1>
+                <h1>Feedback : {interview.feedback ? "Given" : "Not Given"}</h1>
+              </div>
+            )
+          })}
+        </div>
+      </div>
     </div>
+
 
   )
 }
