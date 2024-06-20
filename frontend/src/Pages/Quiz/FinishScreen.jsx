43a39e1e676e69aaf0cf28dcfc3b36e5b7cc69c8
vikram110703 <sainivks4321@gmail.com>
2024-06-21 00:50:32 +0530

    testing finish page

--- frontend/src/Pages/Quiz/FinishScreen.jsx
+++ frontend/src/Pages/Quiz/FinishScreen.jsx
@@ -1,10 +1,105 @@
-import React from 'react'
+import { Button } from 'flowbite-react';
+import React, { useState } from 'react'
 
-export const FinishScreen = ({score,quizQuestions}) => {
+export const FinishScreen = ({ score, quizQuestions, userQuizResponse }) => {
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(1);
+  const [correctOptionIndex, setCorrectOptionIndex] = useState(0);
+  console.log("questions", quizQuestions);
+  score=1;
+  console.log("score",score);
+
+
+  const size = quizQuestions.length;
+
+
+  const correctOptionClass = `bg-green-500`;
+  const wrongOptionClass = `bg-red-500`;
+
+  const handlePreviousClick = () => {
+    if (currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+    else return;
+  };
+
+  const handleNextClick = () => {
+    if (currentQuestionIndex < size - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+    else return;
+  };
+
   return (
-    <div className='flex flex-col items-center gap-5 p-5'>
-        <h1>Quiz Completed</h1>
-        <p>Your score: {score} / {quizQuestions.length}</p>
+    <div className='min-h-screen flex flex-col gap-4  bg-gray-200 dark:bg-gray-800 '>
+      <>
+      <div className='flex justify-center py-2 font-semibold'>Your score: {score}/{size}</div>
+        <div className='  grid grid-cols-1 sm:grid-cols-2  border-t border-b border-gray-400 dark:border-gray-500  '>
+
+          {/* <h1>Quiz</h1> */} 
+          <div className='flex flex-col   gap-3   p-3 bg-gray-300 dark:bg-gray-900'>
+            <h1>Question {currentQuestionIndex + 1}</h1>
+            <h2>{quizQuestions[currentQuestionIndex].description}</h2>
+          </div>
+          <div className='flex flex-col  gap-3   p-3 bg-gray-200 dark:bg-gray-800'>
+            <h2>Answer:</h2>
+            <div className='flex flex-col items-start gap-3'>
+              {quizQuestions[currentQuestionIndex].options.map((option, index) => (
+                <div
+                  // className='w-full'>
+                  className={`w-full  ${(index === selectedOptionIndex && selectedOptionIndex === correctOptionIndex) ? correctOptionClass : ''}
+                  ${(index === selectedOptionIndex && selectedOptionIndex !== correctOptionIndex) ? wrongOptionClass : ''}
+                  ${index === correctOptionIndex ? correctOptionClass : ''} `}>
+                  <div key={index} className='flex gap-2  border border-gray-500 dark:border-gray-100 rounded-md'>
+                    <input className='m-2'
+                      type="radio"
+                      name="option"
+                      checked={selectedOptionIndex === index}
+                    />
+                    <div className='p-2'>
+                      {option}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+          </div>
+
+        </div>
+
+        <div className='flex flex-wrap justify-center p-3 gap-5'>
+          <button className=' px-2 py-1 hover:underline underline-offset-1'
+            onClick={handlePreviousClick}  >Previous</button>
+          <button className=' px-2 py-1 hover:underline underline-offset-1'
+            onClick={handleNextClick}  >Next</button>
+        </div>
+
+        <div className=''>
+          <QuestionsSequence size={size} currentQuestionIndex={currentQuestionIndex} setCurrentQuestionIndex={setCurrentQuestionIndex} />
+        </div>
+
+
+      </>
+      {/* <ToastContainer /> */}
     </div>
-)
-}
+  );
+};
+
+const QuestionsSequence = ({ size, currentQuestionIndex, setCurrentQuestionIndex }) => {
+  return (
+
+    <div className='max-h-40 overflow-y-scroll p-4 flex flex-wrap gap-3'>
+      {Array.from({ length: size }, (_, i) => i).map((index) => (
+        <button
+          key={index}
+          onClick={() => setCurrentQuestionIndex(index)}
+          className={`px-3 py-1 rounded-md text-center border border-gray-400 dark:border-gray-400 ${index === currentQuestionIndex ? 'bg-blue-500' : ''} `}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
--- frontend/src/Pages/Quiz/ParticipateQuiz.jsx
+++ frontend/src/Pages/Quiz/ParticipateQuiz.jsx
@@ -10,52 +10,37 @@ import MyApolloProvider from '../../index';
 import { useDispatch, useSelector } from 'react-redux';
 import { setLoading } from '../../app/user/userSlice';
 import { Loader } from '../Loader';
-import { GET_QUIZ_BY_ID, GET_QUIZ_BY_ID_with_Q, GET_QUIZ_BY_ID_without_Q } from '../../gqlOperatons/Quiz/mutations';
+import { GET_QUIZ_BY_ID, GET_QUIZ_BY_ID_with_Q, GET_QUIZ_BY_ID_without_Q, QUIZ_RESPONSE, SET_QUIZ_RESPONSE } from '../../gqlOperatons/Quiz/mutations';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FinishScreen } from './FinishScreen';
+import { setDefaultLocale } from 'react-datepicker';
 
-// Dummy data for questions
-// const quizQuestions = [
-//     {
-//         description: "What is the capital of France?",
-//         options: ["Berlin", "Madridsjdlk djdjjjjjjjjjjjj c+jajindnfl jdfjdkljkl  jdkjl   c=== disnks isi dsfdskmdkmsfisdkkms i      fidkn (inti i=jjJ++)jdk jdksjkdjdjkdj    djdjkjskjf    djkdjfkdj   ", "Paris", "Rome"],
-//         correctOptionIndex: 2,
-//     },
-//     {
-//         description: "What is 2 + 2?",
-//         options: ["3", "4", "5", "6"],
-//         correctOptionIndex: 1,
-//     },
-//     {
-//         description: "What is the color of the sky?",
-//         options: ["Blue", "Green", "Red", "Yellow"],
-//         correctOptionIndex: 0,
-//     },
-// ];
 
 const ParticipateQuiz = () => {
 
-    const { isLoading } = useSelector((state) => state.user);
+    const { isLoading, id, ready, loggedIn } = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const params = useParams();
     const navigate = useNavigate();
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
-    const [score, setScore] = useState(0);
     const [quizCompleted, setQuizCompleted] = useState(false);
     const [ERROR, setError] = useState(null);
-    const [alreadyAttempted, setAlreadyAttempted] = useState(null);
+    const [alreadyAttempted, setAlreadyAttempted] = useState(false);
     const [quizQuestions, setQuizQuestions] = useState(null);
+    const [userQuizResponse, setUserQuizResponse] = useState(null);
+    const [score, setScore] = useState(0);
 
     const timePerQuestion = 20;
     const [time, setTime] = useState(timePerQuestion);
 
-    const id = params.id;
-    if (isNaN(id)) {
+    let quizId = params.id;
+    if (isNaN(quizId)) {
         navigate('/notFound');
     }
-    const ID = parseInt(id);
+    quizId = parseInt(params.id);
+    const userId = parseInt(id);
 
 
     const [getQuiz] = useMutation(GET_QUIZ_BY_ID_with_Q, {
@@ -65,14 +50,73 @@ const ParticipateQuiz = () => {
         },
     });
 
+    const [setResponse] = useMutation(SET_QUIZ_RESPONSE, {
+        onError: (error) => {
+            console.log("Error from query 1", error);
+            return setError(error);
+        },
+    });
+
+    const [quizResponse] = useMutation(QUIZ_RESPONSE, {
+        onError: (error) => {
+            console.log("Error from query 1", error);
+            return setError(error);
+        },
+    });
+
+
+    // to check if the quiz is already attempted or not 
     useEffect(() => {
+        if (!ready) return;
+        console.log("enter in the hook");
+        (async () => {
+            try {
+                const { data, errors } = await quizResponse({
+                    variables: {
+                        quizId: quizId,
+                        userId: userId,
+                    }
+                });
+                if (errors) {
+                    console.log("error quiz res 1", errors);
+                    return setError(errors);
+                }
+                else if (data) {
+                    console.log("quiz response data", data);
+                    // console.log(data.getQuizResponseForUser===null);
+                    if (data.getQuizResponseForUser === null) {
+                        setAlreadyAttempted(false);
+                    }
+                    else {
+                        setUserQuizResponse(data.getQuizResponseForUser.response);
+                        setScore(data.getQuizResponseForUser.score);
+                        setAlreadyAttempted(true);
+                    }
+                }
+                else {
+                    console.log("kuchh nhi mila");
+                }
+            } catch (error) {
+                console.log("error quiz res 2 ", error);
+                return setError(error);
+            }
+            finally {
+                dispatch(setLoading(false));
+            }
+        })();
+    }, [ready]);
+
+
+    // to fetch the quiz data
+    useEffect(() => {
+        if (!ready) return;
         (
             async () => {
                 dispatch(setLoading(true));
                 try {
                     const { data, errors } = await getQuiz({
                         variables: {
-                            QuizId: ID,
+                            QuizId: quizId,
                         }
                     });
                     if (errors) {
@@ -85,95 +129,98 @@ const ParticipateQuiz = () => {
                         const startTime = moment(data.getQuizById.startTime);
                         const endTime = moment(data.getQuizById.endTime);
                         const currentTime = moment();
-                        if (currentTime < startTime) {
+                        if (moment(startTime).isAfter(currentTime)) {
                             dispatch(setLoading(false));
-                            navigate(`/quiz/id/${ID}`);
+                            navigate(`/quiz/id/${quizId}`);
+                            return null;
                         }
-                        const { data: res, errors: err } = await MyApolloProvider.client.query({
-                            query: GET_USER_TRAINING_QUIZ,
-                            onerror: (error) => {
-                                // console.log("Error from query", error);
-                                dispatch(setLoading(false));
-                                return setError(error);
-                            }
-                        });
-                        if (err) {
-                            console.log("Errors from query 3", err);
-                            dispatch(setLoading(false));
-                            return setError(err);
-                        }
-                        else if (res) {
-                            console.log("Training user result ", res);
-                            res.getMe.userTraining.quizesAttended.map((quiz) => {
-                                if (parseInt(quiz.quizId) === ID) {
-                                    setScore(quiz.score);
-                                    setAlreadyAttempted(true);
-                                    return dispatch(setLoading(false));
+                        // const { data: res, errors: err } = await MyApolloProvider.client.query({
+                        //     query: GET_USER_TRAINING_QUIZ,
+                        //     onerror: (error) => {
+                        //         // console.log("Error from query", error);
+                        //         dispatch(setLoading(false));
+                        //         return setError(error);
+                        //     }
+                        // });
+                        // if (err) {
+                        //     console.log("Errors from query 3", err);
+                        //     dispatch(setLoading(false));
+                        //     return setError(err);
+                        // }
+                        // else if (res) {
+                        //     console.log("Training user result ", res);
+                        //     res.getMe.userTraining.quizesAttended.map((quiz) => {
+                        //         if (parseInt(quiz.quizId) === quizId) {
+                        //             setScore(quiz.score);
+                        //             setAlreadyAttempted(true);
+                        //             return dispatch(setLoading(false));
 
-                                }
-                            })
-                            setQuizQuestions(data.getQuizById.questions);
-                            console.log('quiz question ', data.getQuizById.questions);
-                            dispatch(setLoading(false));
-                        }
-                        else dispatch(setLoading(false));
+                        //         }
+                        //     })
+                        //     setQuizQuestions(data.getQuizById.questions);
+                        //     console.log('quiz question ', data.getQuizById.questions);
+                        //     dispatch(setLoading(false));
+                        // }
+                        // else dispatch(setLoading(false));
+
+                        setQuizQuestions(data.getQuizById.questions);
+                        console.log('quiz questions  ', data.getQuizById.questions);
+                        const tempArray = new Array(data.getQuizById.questions.length).fill(-1);
+                        setUserQuizResponse(tempArray);
                     }
-                    else dispatch(setLoading(false));
 
                 } catch (error) {
                     console.log('error 4', error);
-                    dispatch(setLoading(false));
                     return setError(error);
+                }
+                finally {
+                    dispatch(setLoading(false));
                 }
 
             })
             ();
 
-
-    }, []);
+    }, [ready]);
 
 
 
     const handleOptionClick = (index) => {
         setSelectedOptionIndex(index);
+        setUserQuizResponse((prev) => {
+            const temp = [...prev];
+            temp[currentQuestionIndex] = index + 1;
+            return temp;
+        });
     };
 
-    const handleNextQuestion = () => {
+    const handleNextQuestion = async () => {
         if (selectedOptionIndex === null) {
             toast.error("Please select an option");
             return;
         }
         // console.log("Selected option index", selectedOptionIndex);
         // console.log("Correct option index", quizQuestions[currentQuestionIndex].correctOption);
-        if (selectedOptionIndex+1 === quizQuestions[currentQuestionIndex].correctOption) {
-            setScore(score + 1);
-        }
-
         setSelectedOptionIndex(null);
 
         if (currentQuestionIndex + 1 < quizQuestions.length) {
             setCurrentQuestionIndex(currentQuestionIndex + 1);
             setTime(timePerQuestion);
         } else {
+            handleFinishLogic();
             setTime(0);
-            setQuizCompleted(true);
         }
     };
 
-    const handleTimeUp = () => {
+    const handleTimeUp = async () => {
         // console.log("Time up", currentQuestionIndex, quizQuestions.length);
-        if (selectedOptionIndex !== null) {
-            if (selectedOptionIndex+1 === quizQuestions[currentQuestionIndex].correctOption) {
-                setScore(score + 1);
-            }
-        }
         setSelectedOptionIndex(null);
         if (currentQuestionIndex + 1 < quizQuestions.length) {
+            console.log("dd",selectedOptionIndex,quizQuestions[currentQuestionIndex].correctOption);
             setCurrentQuestionIndex(currentQuestionIndex + 1);
             setTime(timePerQuestion);
         } else {
+            handleFinishLogic();
             setTime(0);
-            setQuizCompleted(true);
         }
 
     };
@@ -184,28 +231,55 @@ const ParticipateQuiz = () => {
         const response = await UserConfirmation("Are you sure to finish the quiz ?");
         console.log("Response from UserConfirmation", response);
         if (response) {
-            if (selectedOptionIndex !== null) {
-                if (selectedOptionIndex+1 === quizQuestions[currentQuestionIndex].correctOption) {
-                    setScore(score + 1);
-                }
-            }
+            handleFinishLogic();
             setSelectedOptionIndex(null);
-            setQuizCompleted(true);
+            setTime(0);
+
         }
-        else { };
+        else return;
 
     }
-    if (isLoading) {
-        return <Loader />;
+
+    const handleFinishLogic = async () => {
+        dispatch(setLoading(true));
+        try {
+            const { data, errors } = await setResponse({
+                variables: {
+                    quizId: quizId,
+                    response: userQuizResponse,
+                }
+            });
+            if (errors) {
+                console.log("error in set response 2", errors);
+                setError(errors);
+            }
+            else if (data) {
+                console.log("quiz submition data", data);
+                toast.success("Quiz submitted successfully");
+                setQuizCompleted(true);
+            }
+        } catch (error) {
+            console.log("error in set response 3", error);
+            setError(error);
+        }
+        finally {
+            dispatch(setLoading(false));
+        }
+    };
+
+    if (isLoading || !ready) return <Loader />;
+    if (ready && (loggedIn === false)) {
+        return navigate('/register');
     }
     if (ERROR) {
-        console.log("error at participateQuiz page ", ERROR);
-        return <div>Error</div>;
+        return toast.error(ERROR.message ? ERROR.message : ERROR || "something went wrong ");
     }
-    // if (alreadyAttempted) {
-    //     return <FinishScreen />
-    // }
 
+    if (ready && alreadyAttempted) {
+        return (
+            <FinishScreen score={userQuizResponse ? userQuizResponse.score : 5} quizQuestions={quizQuestions} userQuizResponse={userQuizResponse} />
+        );
+    }
     return (
         <div className='min-h-screen flex flex-col gap-4  bg-gray-200 dark:bg-gray-800 '>
             {!quizCompleted && quizQuestions?.length > 0 && (
@@ -259,14 +333,6 @@ const ParticipateQuiz = () => {
                     </div>
                 </>
                 // {/* <div>hello</div> */}
-            )}
-
-            {alreadyAttempted && (
-                <FinishScreen score={score} quizQuestions={quizQuestions} />
-            )}
-
-            {quizCompleted && (
-                <FinishScreen score={score} quizQuestions={quizQuestions} />
             )}
 
             {/* <ToastContainer /> */}
