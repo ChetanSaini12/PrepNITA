c01809fe41954606a4bcb507f36ee9b0869a3ab4
vikram110703 <sainivks4321@gmail.com>
2024-06-30 20:58:33 +0530

    mutation added for experience page

---
+++ frontend/src/Components/Loader2.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+
+export const Loader2 = () => {
+    return (
+        <div className="loader-2-container">
+            <div className="loader-2 bg-gray-400 dark:bg-gray-600"></div>
+        </div>
+    )
+}
--- frontend/src/gqlOperatons/Experience/mutations.js
+++ frontend/src/gqlOperatons/Experience/mutations.js
@@ -2,14 +2,29 @@ import { gql } from '@apollo/client';
 
 // export const CREATE_EXPERIENCE = gql``;
 
+export const CREATE_EXPERIENCE = gql`
+    mutation createExperienceMutation($Experience : InputExperience){
+        createExperience(Experience : $Experience){
+            id
+        }
+    }
+`;
+
 export const GET_ALL_EXPERIENCE = gql`
     mutation getAllExperienceMutaiton{
         getAllExperience{
-            id
-            company
-            role
-            anonymous
-            creatorUsername
+         id
+         company
+         role
+         description
+         anonymous
+         createdBy
+         creatorName
+         creatorUsername
+         createdAt
+         upvotes
+         downvotes
+         comments{id}
         }
     }
 `;
@@ -47,3 +62,89 @@ export const GET_EXPERIENCE_BY_ID = gql`
         }
     }
 `;
+
+export const UPVOTE_EXPERIENCE = gql`
+    mutation upvoteExperienceMutation($id : Int!){
+        upvoteExperience(id : $id){
+            id
+            upvotes
+            downvotes
+        }
+    }
+`;
+export const DOWNVOTE_EXPERIENCE = gql`
+    mutation downvoteExperienceMutation($id : Int!){
+        downvoteExperience(id : $id){
+            id
+            upvotes
+            downvotes
+        }
+    }
+`;
+export const ADD_COMMENT_EXP = gql`
+    mutation addCommentExpMutation($Comment : InputExpComment){
+        addCommentExp(Comment : $Comment){
+             id 
+             experienceId
+             description
+                reply{
+                 id
+                }
+                commentorId
+                commentorUserName
+                likes
+        }
+    }
+`;
+export const ADD_REPLY_TO_EXP_COMMENT = gql`
+    mutation addReplyToExpCommentMutation($Reply : InputExpReply){
+        addReplyToExpComment(Reply : $Reply){
+            id
+        }
+    }
+`;
+export const DELETE_EXP_COMMENT = gql`
+    mutation deleteExpCommentMutation($commentId : Int){
+        deleteExpComment(commentId : $commentId){
+            id
+        }
+    }
+`;
+export const DELETE_EXP_REPLY = gql`
+    mutation deleteExpReplyMutation($replyId : Int){
+        deleteExpReply(replyId : $replyId){
+            id
+        }
+    }
+`;
+export const LIKE_EXP_COMMENT = gql`
+    mutation likeExpCommentMutation($commentId : Int){
+        likeExpComment(commentId : $commentId){
+            id
+            likes
+        }
+    }
+`;
+export const LIKE_EXP_COMMENT_REPLY = gql`
+    mutation likeExpCommentReplyMutation($replyId : Int){
+        likeExpCommentReply(replyId : $replyId){
+            id
+            likes
+        }
+    }
+`;
+export const DELETE_EXPERIENCE = gql`
+    mutation deleteExperienceMutation($id : Int!){
+        deleteExperience(id : $id){
+            id
+        }
+    }
+`;
+export const UPDATE_EXPERIENCE = gql`
+    mutation updateExperienceMutation($id : Int!, $Experience : InputExperience){
+        updateExperience(id : $id, Experience : $Experience){
+            id
+        }
+    }
+`;
+
