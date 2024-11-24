fbcd08eed36a6e5e95a185e068ad5299f3a595ae
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2024-11-24 09:01:22 +0530

    refactor: update socket initialization to use INTERVIEW_URL and improve options

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,12 +1,13 @@
-import {io} from 'socket.io-client'
+import { io } from "socket.io-client";
+import { INTERVIEW_URL } from "./utils";
 
-export async function initSocket(){
-    const option={
-        'force new connection':true,
-        recoonectionAttempt:Infinity,
-        'timeout':180000,
-        'transports':['websocket']
-    }
+export async function initSocket() {
+  const options = {
+    "force new connection": true,
+    reconnectionAttempts: 5,
+    timeout: 180000,
+    transports: ["websocket"],
+  };
 
-    return io("http://localhost:7001", option);
+  return io(INTERVIEW_URL, options);
 }
