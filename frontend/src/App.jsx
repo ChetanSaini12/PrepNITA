574f260ea3d382990704e28941219db63b54eed0
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2024-11-24 10:48:59 +0530

    refactor: restructure App component and create MainLayout for improved routing & fix navbar and footer not re-appearing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,9 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard";
-import Home from "./Pages/Home";
-import Header from "./Components/Header";
-import Quizes from "./Pages/Quiz/Quizes.jsx";
-import Discuss from "./Pages/Discuss";
-import SignUp from "./Pages/SignUp";
-import FooterCom from "./Components/Footer";
-import AllUsers from "./Pages/UserPages/AllUsers.jsx";
-import { Profile } from "./Pages/UserPages/Profile.jsx";
-import Question from "./Pages/Questions/Question.jsx";
-import CreateQuestion from "./Pages/Contribute/CreateQuestion.jsx";
-import Onboarding from "./Pages/Onboarding";
-import Interview from "./Pages/InterviewPages/Interview.jsx";
-import InterviewDetail from "./Pages/InterviewPages/InterviewDetail.jsx";
-import PageNotFound from "./Pages/404Page.jsx";
-import QuestionById from "./Pages/Questions/QuestionById.jsx";
-import "particles.js/particles";
-import { ProfileById } from "./Pages/UserPages/ProfileById.jsx";
-import AdminInterview from "./Pages/InterviewPages/adminInterview.jsx";
-import { CreateQuiz } from "./Pages/Contribute/CreateQuiz.jsx";
-import QuizDetail from "./Pages/Quiz/QuizDetail.jsx";
-import ParticipateQuiz from "./Pages/Quiz/ParticipateQuiz.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import ScrollToTop from "./Components/ScrollToTop.jsx";
-import { VerifyToken } from "./utils/verifyToken.js";
-import { LogoutUser, setLoading } from "./app/user/userSlice.jsx";
-import Contribute from "./Pages/Contribute/Contribute.jsx";
-import TextEditor from "./Components/sampleTextEditor.jsx";
-import CreateExperience from "./Pages/Contribute/CreateExperience.jsx";
-import RoleManagement from "./Pages/UserPages/RoleManagement.jsx";
-import MyQuizes from "./Pages/Quiz/MyQuizes.jsx";
-import { AllExperience } from "./Pages/Experience/AllExperience.jsx";
-import { ExperienceById } from "./Pages/Experience/ExperienceById.jsx";
-import PendingApproval from "./Pages/SuperAdmin/PendingApproval.jsx";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainLayout from "./MainLayout";
 
 function App() {
-  const { theme } = useSelector((state) => state.theme);
-  if (theme === "dark") {
-    window.particlesJS.load("particles", "/particlesjs-config.json");
-  }
+  const theme = "dark"; // Replace with your Redux state or theme management logic
 
   return (
     <div className="App">
@@ -48,57 +12,12 @@ function App() {
           {theme === "dark" ? (
             <div
               id="particles"
-              className="w-screen h-screen light:bg-[rgb(0,0,0)] dark:bg-[rgb(16,23,42)] color : "
+              className="w-screen h-screen light:bg-[rgb(0,0,0)] dark:bg-[rgb(16,23,42)]"
             />
-          ) : (
-            <></>
-          )}
+          ) : null}
         </div>
         <BrowserRouter>
-          <ScrollToTop />
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/faltu" element={<AllUsers />}></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/quizes" element={<Quizes />}></Route>
-            <Route path="/quizes/my" element={<MyQuizes/>}></Route>
-            <Route path="/contribute/quiz" element={<CreateQuiz />}></Route>
-            <Route path="/contribute/experience" element={<CreateExperience />}></Route>
-            <Route
-              path="/contribute/question"
-              element={<CreateQuestion />}
-            ></Route>
-            <Route path="/quiz/id/:id" element={<QuizDetail />}></Route>
-            <Route path="/quiz/view/:id" element={<ParticipateQuiz />}></Route>
-
-            <Route path="/discuss" element={<Discuss />}></Route>
-            <Route path="/register" element={<SignUp />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/profile/:username" element={<ProfileById />}></Route>
-            <Route path="/questions" element={<Question />}></Route>
-            <Route path="/questions/:id" element={<QuestionById />}></Route>
-            <Route path="/role_management" element={<RoleManagement />}></Route>
-            <Route path="/students" element={<AllUsers />}></Route>
-            <Route path="/onboarding" element={<Onboarding />}></Route>
-            <Route path="/interview" element={<Interview />}></Route>
-            <Route
-              path="/interviewdummyadmin"
-              element={<AdminInterview />}
-            ></Route>
-            <Route path="/interview/:id" element={<InterviewDetail />}></Route>
-
-            <Route path="/experience" element={<AllExperience/>}></Route>
-            <Route path="/experience/:id" element={<ExperienceById/>}></Route>
-
-            <Route path="/contribute" element={<Contribute />}></Route>
-            <Route path="/texted" element={<TextEditor />}></Route>
-            <Route path="/pending_approval" element={<PendingApproval />}></Route>
-            <Route path="/notFound" element={<PageNotFound />}></Route>
-            <Route path="/*" element={<PageNotFound />}></Route>
-            {/* <Route path="/loader" element={<Loader/>}></Route> */}
-          </Routes>
-          <FooterCom></FooterCom>
+          <MainLayout />
         </BrowserRouter>
       </div>
     </div>
