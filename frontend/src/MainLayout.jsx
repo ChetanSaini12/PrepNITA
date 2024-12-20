6e2b6d7936aa7e54be6a30dfeda6f2d5419be7b5
Chetan Saini <chetansaini1241@gmail.com>
2024-12-20 21:47:52 +0530

    Particle Js Enabled

--- frontend/src/MainLayout.jsx
+++ frontend/src/MainLayout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "./Components/Header";
 import FooterCom from "./Components/Footer";
 import ScrollToTop from "./Components/ScrollToTop";
+import "particles.js/particles";
 
 // Pages
 import Dashboard from "./Pages/Dashboard";
@@ -35,6 +37,12 @@ import PendingApproval from "./Pages/SuperAdmin/PendingApproval.jsx";
 import EditorPage from "./Pages/InterviewPages/EditorPage.js";
 
 const MainLayout = () => {
+
+  const { theme } = useSelector((state) => state.theme);
+  if (theme === "dark") {
+    window.particlesJS.load("particles", "/particlesjs-config.json");
+  }
+
   const location = useLocation();
 
   // Define routes where Header and Footer should be hidden
@@ -45,41 +53,53 @@ const MainLayout = () => {
 
   return (
     <>
-      <ScrollToTop />
-      {!shouldHideNavAndFooter && <Header />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/faltu" element={<AllUsers />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/quizes" element={<Quizes />} />
-        <Route path="/quizes/my" element={<MyQuizes />} />
-        <Route path="/contribute/quiz" element={<CreateQuiz />} />
-        <Route path="/contribute/experience" element={<CreateExperience />} />
-        <Route path="/contribute/question" element={<CreateQuestion />} />
-        <Route path="/quiz/id/:id" element={<QuizDetail />} />
-        <Route path="/quiz/view/:id" element={<ParticipateQuiz />} />
-        <Route path="/editor/:roomId" element={<EditorPage />} />
-        <Route path="/discuss" element={<Discuss />} />
-        <Route path="/register" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/:username" element={<ProfileById />} />
-        <Route path="/questions" element={<Question />} />
-        <Route path="/questions/:id" element={<QuestionById />} />
-        <Route path="/role_management" element={<RoleManagement />} />
-        <Route path="/students" element={<AllUsers />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/interview" element={<Interview />} />
-        <Route path="/interviewdummyadmin" element={<AdminInterview />} />
-        <Route path="/interview/:id" element={<InterviewDetail />} />
-        <Route path="/experience" element={<AllExperience />} />
-        <Route path="/experience/:id" element={<ExperienceById />} />
-        <Route path="/contribute" element={<Contribute />} />
-        <Route path="/texted" element={<TextEditor />} />
-        <Route path="/pending_approval" element={<PendingApproval />} />
-        <Route path="/notFound" element={<PageNotFound />} />
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
-      {!shouldHideNavAndFooter && <FooterCom />}
+      <div className="relative">
+        <div className="fixed top-0 left-0 -z-50">
+          {theme === "light" ? (
+            <div
+              id="particles"
+              className="w-screen h-screen light:bg-[rgb(0,0,0)] dark:bg-[rgb(16,23,42)] color : "
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+        <ScrollToTop />
+        {!shouldHideNavAndFooter && <Header />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faltu" element={<AllUsers />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/quizes" element={<Quizes />} />
+          <Route path="/quizes/my" element={<MyQuizes />} />
+          <Route path="/contribute/quiz" element={<CreateQuiz />} />
+          <Route path="/contribute/experience" element={<CreateExperience />} />
+          <Route path="/contribute/question" element={<CreateQuestion />} />
+          <Route path="/quiz/id/:id" element={<QuizDetail />} />
+          <Route path="/quiz/view/:id" element={<ParticipateQuiz />} />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+          <Route path="/discuss" element={<Discuss />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:username" element={<ProfileById />} />
+          <Route path="/questions" element={<Question />} />
+          <Route path="/questions/:id" element={<QuestionById />} />
+          <Route path="/role_management" element={<RoleManagement />} />
+          <Route path="/students" element={<AllUsers />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/interview" element={<Interview />} />
+          <Route path="/interviewdummyadmin" element={<AdminInterview />} />
+          <Route path="/interview/:id" element={<InterviewDetail />} />
+          <Route path="/experience" element={<AllExperience />} />
+          <Route path="/experience/:id" element={<ExperienceById />} />
+          <Route path="/contribute" element={<Contribute />} />
+          <Route path="/texted" element={<TextEditor />} />
+          <Route path="/pending_approval" element={<PendingApproval />} />
+          <Route path="/notFound" element={<PageNotFound />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+        {!shouldHideNavAndFooter && <FooterCom />}
+      </div>
     </>
   );
 };
